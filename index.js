74a3f5734ef2d36c56963f14c9747c21970df2f0
Sam Marland <smarland@redhat.com>
2018-08-09 14:19:41 +0100

    Finished gif support and added photo support

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const session = require('telegraf/session');
 const GoogleSpreadsheet = require('google-spreadsheet');
 const Dropbox = require('dropbox').Dropbox;
 const https = require('https');
+const fs = require('fs');
 
 // Credentials and other runtime config
 const telegramToken = process.env.TELEGRAM_TOKEN;
@@ -12,6 +13,7 @@ const bot = new Telegraf(telegramToken);
 const dropboxToken = process.env.DROPBOX_TOKEN;
 const creds = require('./creds.json');
 const sheetID = process.env.SHEET_ID;
+const dropboxURL = "https://www.dropbox.com/home";
 
 // Filepaths in DropBox
 const gifPath = "/telegram-bot/gifs";
@@ -65,30 +67,63 @@ bot.hears(/^nominate$/i, (ctx) => {
         if (ctx.message.reply_to_message.animation) {
             fileId = ctx.message.reply_to_message.animation.file_id;
             ctx.telegram.getFile(fileId).then(function (response) {
-                var file = fs.createWriteStream("file.mp4");
+                fileName = fileId + ".mp4";
+                var file = fs.createWriteStream(fileName);
                 var request = https.get("https://api.telegram.org/file/bot" + telegramToken + "/" + response.file_path, function (response) {
                     response.pipe(file).on("finish", function () {
-                        uploadFile = fs.readFileSync('file.mp4');
+                        uploadFile = fs.readFileSync(fileName);
                         dbx.filesUpload({
-                                path: gifPath + "/" + fileId + ".mp4",
+                                path: gifPath + "/" + fileName,
                                 contents: uploadFile,
                                 autorename: true
                             })
                             .then(function (response) {
-                                console.log(response);
+                                data.linkToGif = dropboxURL + response.path_lower;
+                                addToSpreadSheet(data);
+                                ctx.replyWithMarkdown(`Thanks for your nomination. It's safely stored in a database that Karl can't get to`);
+                                console.log("Message the was nominated was " + ctx.message.reply_to_message.text + "\nNominated By " + ctx.message.reply_to_message.from.first_name + " " + ctx.message.reply_to_message.from.last_name);
                             })
                             .catch(function (error) {
                                 console.error(error);
                             });
+                        fs.unlinkSync(fileName);
                     });
                 });
 
             });
+        } else if (ctx.message.reply_to_message.photo) {
+            // This is where the photo logic happens
+            fileId = ctx.message.reply_to_message.photo[4].file_id;
+            ctx.telegram.getFile(fileId).then(function (response) {
+                extenstion = response.file_path.split(".")[1];
+                fileName = response.file_id + "." + extenstion;
+                var file = fs.createWriteStream(fileName);
+                var request = https.get("https://api.telegram.org/file/bot" + telegramToken + "/" + response.file_path, function (response) {
+                    response.pipe(file).on("finish", function () {
+                        uploadFile = fs.readFileSync(fileName);
+                        dbx.filesUpload({
+                                path: photosPath + "/" + fileName,
+                                contents: uploadFile,
+                                autorename: true
+                            })
+                            .then(function (response) {
+                                data.linkToPhoto = dropboxURL + response.path_lower;
+                                addToSpreadSheet(data);
+                                ctx.replyWithMarkdown(`Thanks for your nomination. It's safely stored in a database that Karl can't get to`);
+                                console.log("Message the was nominated was " + ctx.message.reply_to_message.text + "\nNominated By " + ctx.message.reply_to_message.from.first_name + " " + ctx.message.reply_to_message.from.last_name);
+                            })
+                            .catch(function (error) {
+                                console.error(error);
+                            });
+                        fs.unlinkSync(fileName);
+                    });
+                });
+            });
+        } else {
+            addToSpreadSheet(data);
+            ctx.replyWithMarkdown(`Thanks for your nomination. It's safely stored in a database that Karl can't get to`);
+            console.log("Message the was nominated was " + ctx.message.reply_to_message.text + "\nNominated By " + ctx.message.reply_to_message.from.first_name + " " + ctx.message.reply_to_message.from.last_name);
         }
-
-        addToSpreadSheet(data);
-        ctx.replyWithMarkdown(`Thanks for your nomination. It's safely stored in a database that Karl can't get to`);
-        console.log("Message the was nominated was " + ctx.message.reply_to_message.text + "\nNominated By " + ctx.message.reply_to_message.from.first_name + " " + ctx.message.reply_to_message.from.last_name);
     }
 
 });
