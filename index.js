8ffacf01f280cf572528af0625646c14a47be6ff
Sam Marland <smarland@redhat.com>
2021-10-02 19:54:26 +0100

    Fixed some out of date packages and updated the code to work with the new versions

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 require('dotenv').config();
 
 // packages
-const Telegraf = require('telegraf');
+const { Telegraf, session } = require('telegraf');
 const fetch = require('isomorphic-fetch');
-const session = require('telegraf/session');
-const GoogleSpreadsheet = require('google-spreadsheet');
+// const { session } = require('telegraf/session')
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 const Dropbox = require('dropbox').Dropbox;
 const https = require('https');
 const fs = require('fs');
@@ -29,12 +29,11 @@ const photosPath = "/telegram-bot/photos";
 // Google piece based on this tutorial from Twillio - https://www.twilio.com/blog/2017/03/google-spreadsheets-and-javascriptnode-js.html
 // Create a document object using the ID of the spreadsheet - obtained from its URL.
 const doc = new GoogleSpreadsheet(sheetID);
+
+
 var dbx = new Dropbox({
     accessToken: dropboxToken
 });
-
-// // Register session middleware
-bot.use(session());
 
 // Register logger middleware
 bot.use((ctx, next) => {
@@ -185,22 +184,13 @@ const saveNomination = function(ctx, data) {
     });
 };
 
-const updateSpreadsheet = function(data) {
-    return new Promise((resolve, reject) => {
-        // Authenticate with the Google Spreadsheets API.
-        doc.useServiceAccountAuth(creds, function(err) {
-            if (err) {
-                error(err);
-                return reject(err);
-            }
-            // Add a row
-            doc.addRow(1, data, function(err) {
-                if (err) {
-                    error(err);
-                    return reject(err);
-                }
-                return resolve();
-            });
-        });
-    });
+const updateSpreadsheet = async function(data) {
+
+    await doc.useServiceAccountAuth(creds);
+
+    await doc.loadInfo();
+
+    const sheet = doc.sheetsByIndex[0];
+
+    const row = await sheet.addRow(data);
 };
